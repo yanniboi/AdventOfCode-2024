622fc60f75f4c1f07c4072348760336f5bb4dcac
yanniboi <yan@yanniboi.com>
2024-12-07 07:50:13 +0000

    Day 7 example

--- Challenges/Day7.cs
+++ Challenges/Day7.cs
@@ -3,6 +3,8 @@ namespace AdventOfCode.Challenges;
 
 public class Day7 : BaseChallenge
 {
+    public Dictionary<int, List<int>> _equations = new Dictionary<int, List<int>>();
+    
     protected override string GetExampleFilePath()
     {
         return Path.Combine(AppContext.BaseDirectory, "day-7/example/input.txt");
@@ -15,8 +17,21 @@ public class Day7 : BaseChallenge
 
     protected override void SolveStep1()
     {
-        // Do something.
-        _total = 7;
+        // Take equation.
+        // Loop over parts.
+        // Apply both operators.
+        // Check if result is in possible outcomes.
+        foreach (var equation in _equations)
+        {
+            var outcomes = ApplyOperators(equation.Value);
+
+            if (outcomes.Contains(equation.Key))
+            {
+                _total += equation.Key;
+            }
+        }
+        
+        // _total = 3749;
     }
 
     protected override void SolveStep2()
@@ -25,11 +40,44 @@ public class Day7 : BaseChallenge
         _total = 7;
     }
 
+    public List<int> ApplyOperators(List<int> parts)
+    {
+        var progress = new List<int>();
+
+        foreach (var part in parts)
+        {
+            if (progress.Count == 0)
+            {
+                progress.Add(part);
+                continue;
+            }
+
+            var newProgress = new List<int>();
+            foreach (var current in progress)
+            {
+                newProgress.Add(current + part);
+                newProgress.Add(current * part);
+            }
+
+            progress = newProgress;
+        }
+        
+        progress.Sort();
+        return progress;
+    }
+
     public override void ParseInput(string filePath)
     {
         base.ParseInput(filePath);
 
-        // Extra parsing...
+        foreach (var line in _rawLines)
+        {
+            var parts = line.Split(':', StringSplitOptions.TrimEntries);
+            var result = int.Parse(parts[0]);
+            var subparts = parts[1].Split(' ', StringSplitOptions.TrimEntries);
+
+            _equations.Add(result, subparts.Select(x => int.Parse(x)).ToList());
+        }
     }
 
 }
--- Tests/UnitTestDay7.cs
+++ Tests/UnitTestDay7.cs
@@ -12,7 +12,7 @@ public class UnitTestDay7
     {
         var challenge = new Day7();
         var result = challenge.RunExample(1);
-        result.ShouldBe(7);
+        result.ShouldBe(3749);
     }
 
     [TestMethod]
@@ -21,6 +21,50 @@ public class UnitTestDay7
         var challenge = new Day7();
         var result = challenge.RunExample(2);
         result.ShouldBe(7);
+    }
+    
+    [TestMethod]
+    public void Test_Parse()
+    {
+        var challenge = new Day7();
+        var path = Path.Combine(AppContext.BaseDirectory, "day-7/example/input.txt");
+        challenge.ParseInput(path);
+        
+        challenge._equations.Count.ShouldBe(9);
+        challenge._equations.Keys.ToList().Contains(1).ShouldBe(false);
+        challenge._equations.Keys.ToList().Contains(7290).ShouldBe(true);
+        challenge._equations[7290].Contains(1).ShouldBe(false);
+        challenge._equations[7290].Contains(8).ShouldBe(true);
+        challenge._equations[7290].Count().ShouldBe(4);
+        challenge._equations[7290].Where(x => x == 6).ToList().Count.ShouldBe(2);
+    }
+    
+    
+    [TestMethod]
+    [DynamicData(nameof(Test_ApplyOperators_Data), DynamicDataSourceType.Method)]
+    public void Test_ApplyOperators(List<int> parts, List<int> expected)
+    {
+        var challenge = new Day7();
+        expected.Sort();
+
+        var result = challenge.ApplyOperators(parts);
+       
+        result.ShouldBe(expected);
+    }
+
+    public static IEnumerable<object[]> Test_ApplyOperators_Data()
+    {
+        // No obstacle
+        yield return new object[]
+        {
+            new List<int>() {2,4},
+            new List<int>() {6,8},
+        };
+        yield return new object[]
+        {
+            new List<int>() {2,4,6},
+            new List<int>() {12,48,36,14},
+        };
     }
 }
 
